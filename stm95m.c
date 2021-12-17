781f956773774aeaab101057b0508256ea561357
Tobias <egtobi@gmail.com>
2021-12-17 20:43:32 +0100

    Fix

--- stm95m.c
+++ stm95m.c
@@ -15,6 +15,7 @@
  */
 
 #include <stddef.h>
+#include <stdbool.h>
 #include "stm95m.h"
 
 #define WREN    0b00000110
@@ -30,7 +31,7 @@
 
 #define WIP     0x01
 
-memory_status_t stm95m_check_handle(const stm95m_handle_t *const handle) {
+static memory_status_t stm95m_check_handle(const stm95m_handle_t *const handle) {
     if (handle == NULL) {
         return MEMORY_STATUS_INVALID_HANDLE;
     }
@@ -49,12 +50,7 @@ memory_status_t stm95m_check_handle(const stm95m_handle_t *const handle) {
     return MEMORY_STATUS_OK;
 }
 
-memory_status_t stm95m_read(stm95m_handle_t *handle, uint32_t address, uint8_t *data, uint32_t length, size_t cs) {
-    memory_status_t rc = stm95m_check_handle(handle);
-    if (rc != MEMORY_STATUS_OK) {
-        return rc;
-    }
-
+static memory_status_t stm95m_atomic_read(stm95m_handle_t *handle, uint32_t address, uint8_t *data, uint32_t length, size_t cs) {
     uint8_t header[4] = {0};
     header[0] = READ;
     header[1] = address >> 16;
@@ -66,33 +62,41 @@ memory_status_t stm95m_read(stm95m_handle_t *handle, uint32_t address, uint8_t *
         handle->cs_disable(handle->low_level_handle, cs);
         return MEMORY_STATUS_NOK;
     }
-    rc = handle->read(handle->low_level_handle, data, length);
+    uint8_t rc = handle->read(handle->low_level_handle, data, length);
     handle->cs_disable(handle->low_level_handle, cs);
 
     return rc;
 }
 
-memory_status_t stm95m_write_latch_enable(stm95m_handle_t *handle, size_t cs) {
-    uint8_t enable = WREN;
+memory_status_t stm95m_read(stm95m_handle_t *handle, uint32_t address, uint8_t *data, uint32_t length, size_t cs) {
+    memory_status_t rc = stm95m_check_handle(handle);
+    if (rc != MEMORY_STATUS_OK) {
+        return rc;
+    }
 
+    return stm95m_atomic_read(handle, address, data, length, cs);
+}
+
+static memory_status_t stm95m_atomic_write_latch(stm95m_handle_t *handle, uint8_t enable, size_t cs) {
     handle->cs_enable(handle->low_level_handle, cs);
     memory_status_t rc = handle->write(handle->low_level_handle, &enable, 1);
     handle->cs_disable(handle->low_level_handle, cs);
-
     return rc;
 }
 
-memory_status_t stm95m_write_latch_disable(stm95m_handle_t *handle, size_t cs) {
+memory_status_t stm95m_atomic_write_latch_enable(stm95m_handle_t *handle, size_t cs) {
+    uint8_t enable = WREN;
+
+    return stm95m_atomic_write_latch(handle, enable, cs);
+}
+
+memory_status_t stm95m_atomic_write_latch_disable(stm95m_handle_t *handle, size_t cs) {
     uint8_t disable = WRDI;
 
-    handle->cs_enable(handle->low_level_handle, cs);
-    memory_status_t rc = handle->write(handle->low_level_handle, &disable, 1);
-    handle->cs_disable(handle->low_level_handle, cs);
-
-    return rc;
+    return stm95m_atomic_write_latch(handle, disable, cs);
 }
 
-memory_status_t stm95m_wait_wip_completed(stm95m_handle_t *handle, size_t cs) {
+memory_status_t stm95m_atomic_wait_wip_completed(stm95m_handle_t *handle, size_t cs) {
     uint8_t wip = WIP;
     while (wip & WIP) {
         if (stm95m_read_register(handle, &wip, cs) != MEMORY_STATUS_OK) {
@@ -102,28 +106,12 @@ memory_status_t stm95m_wait_wip_completed(stm95m_handle_t *handle, size_t cs) {
     return MEMORY_STATUS_OK;
 }
 
-memory_status_t
-stm95m_write(stm95m_handle_t *handle, uint32_t address, const uint8_t *data, uint32_t length, size_t cs) {
-    memory_status_t rc = stm95m_check_handle(handle);
-    if (rc != MEMORY_STATUS_OK) {
-        return rc;
-    }
-
+static memory_status_t stm95m_atomic_write(stm95m_handle_t *handle, uint32_t address, const uint8_t *data, uint32_t length, size_t cs) {
     uint8_t header[4] = {0};
     header[0] = WRITE;
     header[1] = address >> 16;
     header[2] = address >> 8;
     header[3] = address;
-
-    // Reset WIP-bit in status register
-    uint8_t wip;
-    stm95m_read_register(handle, &wip, cs);
-    wip &= ~WIP;
-    stm95m_write_register(handle, wip, cs);
-
-    if (stm95m_write_latch_enable(handle, cs) != MEMORY_STATUS_OK) {
-        return MEMORY_STATUS_NOK;
-    }
 
     handle->cs_enable(handle->low_level_handle, cs);
 
@@ -131,20 +119,32 @@ stm95m_write(stm95m_handle_t *handle, uint32_t address, const uint8_t *data, uin
     if (handle->write(handle->low_level_handle, header, sizeof header) != MEMORY_STATUS_OK) {
         // If sending the header fails, we need to disable the latch and chip select
         handle->cs_disable(handle->low_level_handle, cs);
-        stm95m_write_latch_disable(handle, cs);
         return MEMORY_STATUS_NOK;
     }
 
     // Send the data
-    rc = handle->write(handle->low_level_handle, data, length);
+    uint8_t rc = handle->write(handle->low_level_handle, data, length);
     handle->cs_disable(handle->low_level_handle, cs);
+    return rc;
+}
 
-    if (stm95m_write_latch_disable(handle, cs) != MEMORY_STATUS_OK) {
+memory_status_t
+stm95m_write(stm95m_handle_t *handle, uint32_t address, const uint8_t *data, uint32_t length, size_t cs) {
+    memory_status_t rc = stm95m_check_handle(handle);
+    if (rc != MEMORY_STATUS_OK) {
+        return rc;
+    }
+
+    if (stm95m_atomic_write_latch_enable(handle, cs) != MEMORY_STATUS_OK) {
         return MEMORY_STATUS_NOK;
     }
 
-    // wait for the write to complete
-    if (stm95m_wait_wip_completed(handle, cs) != MEMORY_STATUS_OK) {
+    if (stm95m_atomic_write(handle, address, data, length, cs) != MEMORY_STATUS_OK) {
+        stm95m_atomic_write_latch_disable(handle, cs);
+        return MEMORY_STATUS_NOK;
+    }
+
+    if (stm95m_atomic_wait_wip_completed(handle, cs) != MEMORY_STATUS_OK) {
         return MEMORY_STATUS_NOK;
     }
 
